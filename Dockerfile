24a75d7846c3aac3f8cf13bda1fa0204b4d1239b
Justin <justinedwards1230@gmail.com>
2024-04-20 19:47:38 -0400

    chore: separate config directory from download directory

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,13 @@
 FROM oven/bun:1 as base
-WORKDIR /usr/src/app
+WORKDIR /app
 
-# install dependencies into temp directory
-# this will cache them and speed up future builds
 FROM base AS install
 RUN mkdir -p /temp/dev
 COPY package.json bun.lockb /temp/dev/
-RUN cd /temp/dev && bun install --frozen-lockfile
+RUN cd /temp/dev && bun install
 
-# copy node_modules from temp directory
-# then copy all (non-ignored) project files into the image
 FROM base AS release
 COPY --from=install /temp/dev/node_modules ./node_modules
 COPY . .
 
-# run the app
 ENTRYPOINT [ "bun", "run", "service.ts" ]
--- src/lib/Directories.ts
+++ src/lib/Directories.ts
@@ -1,27 +1,25 @@
 import path from "path";
 import fs from "fs";
 
-//const CURRENT_DIR = process.cwd();
 const PROJECT_DIR = path.join(__dirname, "..", "..");
-const CURRENT_DIR = path.join(PROJECT_DIR, "data");
 
-const LOGS_DIR = path.join(CURRENT_DIR, "logs");
+// Downloads
+const DOWNLOAD_DIR = path.join(PROJECT_DIR, "downloads");
 
-const DOWNLOAD_DIR = path.join(CURRENT_DIR, "downloads");
+// App data
+const APP_DIR = path.join(PROJECT_DIR, "data");
+const LOGS_DIR = path.join(APP_DIR, "logs");
+const CONFIG_DIR = path.join(APP_DIR, "config");
 
-const STATE_DIR = path.join(CURRENT_DIR, "config");
-const STATE_FILE = "state.json";
-const STATE_PATH = `${STATE_DIR}/${STATE_FILE}`;
-
-const CONFIG_DIR = path.join(CURRENT_DIR, "config");
-const CONFIG_FILE = "config.json";
-const CONFIG_PATH = `${CONFIG_DIR}/${CONFIG_FILE}`;
-
-const dirs = [CURRENT_DIR, LOGS_DIR, DOWNLOAD_DIR, STATE_DIR, CONFIG_DIR];
+// Create directories if they don't exist
+const dirs = [APP_DIR, LOGS_DIR, DOWNLOAD_DIR, CONFIG_DIR];
 for (const dir of dirs) {
 	if (!fs.existsSync(dir)) {
 		fs.mkdirSync(dir);
 	}
 }
+
+const STATE_PATH = `${CONFIG_DIR}/state.json`;
+const CONFIG_PATH = `${CONFIG_DIR}/config.json`;
 
 export { LOGS_DIR, DOWNLOAD_DIR, STATE_PATH, CONFIG_PATH };
